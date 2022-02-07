c59960053ba349c66cb4fe7166c97208f3776ecd
Kolesnikov <jesusimtoday@gmail.com>
2022-02-07 14:39:37 +0700

    Add TextEditor to new Movie creator

--- SwiftUIVersion/UI/Scense/CreateNewMovies/CreateNewMoviesView.swift
+++ SwiftUIVersion/UI/Scense/CreateNewMovies/CreateNewMoviesView.swift
@@ -26,6 +26,18 @@ struct CreateNewMoviesView: View {
                     .textFieldStyle(.roundedBorder)
             }
             .padding(.horizontal, 5)
+            VStack(alignment: .center) {
+                Text("Movie description:")
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                TextEditor(text: $presenter.title)
+                    .padding(.bottom)
+                    .disableAutocorrection(true)
+                    .overlay {
+                        RoundedRectangle(cornerRadius: 12)
+                            .stroke(.gray, lineWidth: 1)
+                    }
+            }
+            .padding([.leading, .bottom, .trailing], 5)
         }.navigationTitle("Create movies")
     }
 }
